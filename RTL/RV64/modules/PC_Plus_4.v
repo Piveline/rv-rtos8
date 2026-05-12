167bbb4cf8a7ace3de61692e707e3e8e8b00767e
khwl <hwctech1026@gmail.com>
2026-05-12 10:52:07 +0900

    [Feat] Revise PC_Plus_4 with 64-bit XLEN

--- RTL/RV64/modules/PC_Plus_4.v
+++ RTL/RV64/modules/PC_Plus_4.v
@@ -1,6 +1,6 @@
 module PCPlus4 (
-    input [31:0] pc,        // Current pc value
-	output [31:0] pc_plus_4 // pc+4 value
+    input [63:0] pc,        // Current pc value
+	output [63:0] pc_plus_4 // pc+4 value
 );
     assign pc_plus_4 = pc + 4;
 
